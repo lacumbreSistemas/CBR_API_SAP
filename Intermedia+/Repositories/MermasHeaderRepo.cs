622c0b2995c554af9ecc4a95d41e73f2bbb9ed88
MarioTabora777 <46462339+MarioTabora777@users.noreply.github.com>
2022-04-01 13:50:22 -0600

    Update MermasHeaderRepo.cs

--- Intermedia+/Repositories/MermasHeaderRepo.cs
+++ Intermedia+/Repositories/MermasHeaderRepo.cs
@@ -9,6 +9,12 @@ namespace Intermedia_.Repositories
   public  class MermasHeaderRepo:MasterRespository
     {
 
+        public cbr_MermasHeader obtenerDocumentoIntermedioMerma(int number)
+        {
+
+            cbr_MermasHeader merma = db.cbr_MermasHeader.FirstOrDefault(i => i.number == number);
+
+        }
         public cbr_MermasHeader crearDocumentoIntermedioMerma(cbr_MermasHeader merma) {
 
             db.cbr_MermasHeader.Add(merma);
