639f6bfcdab909350cac81af8e2db786ceb5f5d1
MarioTabora777 <46462339+MarioTabora777@users.noreply.github.com>
2022-04-28 09:50:34 -0600

    fdsaf
    sdafsd

---
+++ SAP/Repositories/OrdenProduccionSAP.cs
@@ -0,0 +1,40 @@
+﻿using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAP.Repositories
+{
+   public class OrdenProduccionSAP
+    {
+
+        MasterRepository _MasterRepository = MasterRepository.GetInstance();
+
+
+        public void generarOrdenProduccion() {
+            Documents solicitudDevolicion = _MasterRepository.connection.GetBusinessObject(BoObjectTypes.oInventoryGenEntry);
+           
+            Document_Lines lines = solicitudDevolicion.Lines;
+          
+            lines.BaseEntry = 11198;
+            lines.Quantity = 1;
+            lines.Price = 23.3;
+            lines.Add();
+
+          int i =  solicitudDevolicion.Add();
+
+            string nuevaSolicitudDevolucion;
+            if (i == 0)
+            {
+
+                nuevaSolicitudDevolucion = _MasterRepository.connection.GetNewObjectKey();
+            }
+            else {
+                nuevaSolicitudDevolucion = _MasterRepository.connection.GetLastErrorDescription();
+            }
+
+        }
+    }
+}
---
+++ TEST/Controllers/OrdenProduccionController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdenProduccionController : ControllerBase
+    {
+
+        OrdenProduccionRepo ordenProduccion = new OrdenProduccionRepo(); 
+        [HttpGet]
+        public IActionResult GenerarOrdenProduccion()
+        {
+
+            ordenProduccion.generarOrdenProduccion();
+
+            return Ok();
+        }
+    }
+}
